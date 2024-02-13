1d8f8b113fda42ac1d812bb31e8de0b638cc0df6
Henry He <henry.he@betterworks.com>
2024-02-13 16:51:01 -0700

    fix: ignore some tags

--- private.tf
+++ private.tf
@@ -43,7 +43,6 @@ resource "aws_subnet" "private" {
   lifecycle {
     # Ignore tags added by kops or kubernetes
     ignore_changes = [
-      tags["kubernetes.io/cluster/eks"],
       tags.kubernetes,
       tags.SubnetType,
     ]
